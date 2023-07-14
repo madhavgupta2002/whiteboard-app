0647f07f0ce733fc5efacc2a162744a0781de399
Madhav Gupta <mgups2021@gmail.com>
2023-07-14 14:09:38 +0530

    first commit

---
+++ ui/whiteboard-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Container from './container/Container';
+
+
+function App() {
+  return (
+    <Container />
+    );
+}
+
+export default App;
---
+++ ui/whiteboard-app/src/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './style.css';
+
+class Header extends React.Component
+{
+    constructor(props) {
+    }
+
+    render() {
+
+        return (
+            <h1>Collaborative Whiteboard App</h1>
+        )
+    }
+}
+
+export default Header
